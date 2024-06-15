95f8e1c04e554449e7a463ca8c5765329890ec6d
Darkthruster <alphalord01@yahoo.com>
2024-06-15 11:53:44 +0530

    hi

--- dataset_maker.py
+++ dataset_maker.py
@@ -2,6 +2,8 @@
 This file process the video file to create the dataset
 '''
 from video_utils import video_browser
+import cv2
+import numpy as np
 
 
 class datasetmaker:
@@ -16,3 +18,25 @@ class datasetmaker:
         browser = video_browser(filename)
         points = browser.collect_points()
         print(points)
+        return points
+
+    def apply_perspective_transform(frame, points):
+        
+        assert len(points)==4,"no of points not equal to 4"
+
+        # Define the destination points for the perspective transform
+        width = np.sqrt((points[0]-points[1])**2)
+        height = np.sqrt((points[2]-points[1]**2))
+        
+        dst_points = np.float32([[0, 0], [width, 0], [width, height], [0, height]])
+
+        # Convert points to numpy array format
+        src_points = np.float32(points)
+
+        # Compute the perspective transform matrix
+        Mat = cv2.getPerspectiveTransform(src_points, dst_points)
+
+        # Apply the perspective transform to the frame
+        warped_image = cv2.warpPerspective(frame, Mat, (width, height))
+
+        return warped_image
